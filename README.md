f6abc7906d892934fa82d8c9dd082126322a031c
Miguel Guimaraes <miguel.guimaraes053722@gmail.com>
2025-01-15 09:18:59 +0000

    chore(CoachesList_Content): Adding global style and content to App. Add content, style and routing to Header

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ README FILE
 
 1. -> Add routes, done
 2. -> Add pages to route, done
+3. -> Add global style to App, Add Content, Style and Routing to Header
--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
 
 const router = createRouter({
   history: createWebHistory(),
